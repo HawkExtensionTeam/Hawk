bb4196b751651d9bb6f5686189e6a0f7da345658
William Hunt <2620286h@student.gla.ac.uk>
2023-11-14 21:12:01 +0000

    added local chrome storage for tasks

--- popup.js
+++ popup.js
@@ -1,5 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("newTabButton").addEventListener("click", function () {
-      chrome.tabs.create({ url: "new_tab.html" });
-    });
+  document.getElementById("newTabButton").addEventListener("click", function () {
+    chrome.tabs.create({ url: "new_tab.html" });
   });
+
+  // prevent errors where popup.js is used on a page other than new_tab
+  if (document.getElementById("todoForm")) {
+    todoForm.addEventListener("submit", function(event) {
+      // prevents default page reload
+      event.preventDefault();
+  
+      const newTask = document.getElementById("taskInput").value;
+      // prevent empty tasks being added accidentally
+      if (newTask.length > 0) {
+        chrome.storage.local.get({ 'tasks': [] }, function(result) {
+          // avoid pushing to undefined if there are no previous tasks
+          const existingTasks = result.tasks || [];
+          existingTasks.push(newTask);
+          // don't sync with other machines - extension is local
+          chrome.storage.local.set({ 'tasks': existingTasks }, function() {
+            console.log('existingTasks', existingTasks);
+            console.log('newTask', newTask);
+          });
+        });
+      }
+    });
+  }
+});
