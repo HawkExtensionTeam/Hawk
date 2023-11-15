41230931eb47f4b93073bfe7d96690d47f5481c1
Zhixiang <2616718f@student.gla.ac.uk>
2023-11-15 20:47:44 +0000

    Fixed issue #44: All converted to jQuery

--- todo_make.js
+++ todo_make.js
@@ -1,23 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const todoForm = document.getElementById("todoForm");
+$(document).ready(function() {
     setTime();
-    todoForm.addEventListener("submit", function (event) {
+
+    $("#todoForm").submit(function(event) {
         // prevents default page reload
         event.preventDefault();
 
-        const taskTitle = document.getElementById("taskInput").value;
-        const taskDescription = document.getElementById("descriptionInput").value;
-        const taskDate = document.getElementById("dateInput").value;
-        const taskTime = document.getElementById("timeInput").value;
+        const taskTitle = $("#taskInput").val();
+        const taskDescription = $("#descriptionInput").val();
+        const taskDate = $("#dateInput").val();
+        const taskTime = $("#timeInput").val();
 
         // require
         if (taskTitle !== '' && taskDescription !== '' && taskDate !== '' && taskTime !== '') {
-            chrome.storage.local.get({'tasks': []}, function (result) {
+            chrome.storage.local.get({'tasks': []}, function(result) {
                 // avoid pushing to undefined if there are no previous tasks
                 const existingTasks = result.tasks || [];
                 existingTasks.push(taskTitle);
                 // don't sync with other machines - extension is local
-                chrome.storage.local.set({'tasks': existingTasks}, function () {
+                chrome.storage.local.set({'tasks': existingTasks}, function() {
                     console.log('existingTasks', existingTasks);
                     console.log('newTask', taskTitle);
                 });
@@ -28,18 +28,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function setTime() {
     const dateObject = new Date();
-    const date = dateObject.getDate() + "/"
-        + (dateObject.getMonth() + 1) + "/"
-        + dateObject.getFullYear()
+    const date =
+        dateObject.getDate() + "/" +
+        (dateObject.getMonth() + 1) + "/" +
+        dateObject.getFullYear();
 
-    const time =+ dateObject.getHours() + ":"
-        + dateObject.getMinutes() + ":"
-        + dateObject.getSeconds();
+    const time =
+        dateObject.getHours() + ":" +
+        dateObject.getMinutes() + ":" +
+        dateObject.getSeconds();
 
-    // Get the input element
-    const timeElement = document.getElementById('timeInput');
-    const dateElement = document.getElementById('dateInput');
-    // Set the default value
-    timeElement.value = time;
-    dateElement.value = date;
+    // Set the default value using jQuery
+    $("#timeInput").val(time);
+    $("#dateInput").val(date);
 }
