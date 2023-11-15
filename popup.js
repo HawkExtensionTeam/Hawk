51805704c2eafe1579529ce09350192c75fac887
Zhixiang Feng <2616718f@student.gla.ac.uk>
2023-11-15 12:56:05 +0000

    Solved conflicts with the updated code

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+
   document.getElementById("new-tab-button").addEventListener("click", function () {
     chrome.tabs.create({ url: "new_tab.html" });
   });
