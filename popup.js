263c9ac56bc2512a0d119ee1300f603eaee9afe1
William Hunt <2620286h@student.gla.ac.uk>
2023-11-15 12:12:22 +0000

    Solves #37 - html inputs and js consts added

--- popup.js
+++ popup.js
@@ -9,17 +9,21 @@ document.addEventListener("DOMContentLoaded", function () {
       // prevents default page reload
       event.preventDefault();
   
-      const newTask = document.getElementById("taskInput").value;
-      // prevent empty tasks being added accidentally
-      if (newTask.length > 0) {
+      const taskTitle = document.getElementById("taskInput").value;
+      const taskDescription = document.getElementById("taskDescription").value;
+      const taskDate = document.getElementById("taskDate").value;
+      const taskTime = document.getElementById("taskTime").value;
+
+      // require
+      if (taskTitle.length > 0 && taskDescription > 0 && taskDate > 0 && taskTime > 0) {
         chrome.storage.local.get({ 'tasks': [] }, function(result) {
           // avoid pushing to undefined if there are no previous tasks
           const existingTasks = result.tasks || [];
-          existingTasks.push(newTask);
+          existingTasks.push(taskTitle);
           // don't sync with other machines - extension is local
           chrome.storage.local.set({ 'tasks': existingTasks }, function() {
             console.log('existingTasks', existingTasks);
-            console.log('newTask', newTask);
+            console.log('newTask', taskTitle);
           });
         });
       }
