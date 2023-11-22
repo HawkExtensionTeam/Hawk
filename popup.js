d66ac8bdcbaf7f02a8eda088fc60cf70c87c0ae2
Zhixiang Feng <2616718f@student.gla.ac.uk>
2023-11-22 12:30:00 +0000

    Fixed issue#46: Scripts only run on extension pages

--- popup.js
+++ popup.js
@@ -1,24 +1,25 @@
-$(document).ready(function() {
-    $("#new-tab-button").click(function() {
-        chrome.tabs.create({ url: "new_tab.html" });
-    });
+if (window.location.href.startsWith(chrome.runtime.getURL(''))) {
+    $(document).ready(function () {
+        $("#new-tab-button").click(function () {
+            chrome.tabs.create({url: "new_tab.html"});
+        });
 
-    chrome.storage.local.get({ 'tasks': [] }, function(result) {
-        // avoid pushing to undefined if there are no previous tasks
-        const existingTasks = result.tasks || [];
-        var i;
-        if (existingTasks.length === 0) {
-            $("#checklist").append('<h1>There are no tasks!</h1>');
-        }
-        else {
-            for (i = 0; i < existingTasks.length; i++) {
-                console.log(existingTasks[i]);
-                $("#checklist").append('<li class="list-group-item"> <div class="form-check">' +
-                    '<input type="checkbox" class="form-check-input" id="item1">' +
-                    '<label class="form-check-label" for="item1">' + existingTasks[i] + '</label>' +
-                    '</div>' +
-                    '</li>');
+        chrome.storage.local.get({'tasks': []}, function (result) {
+            // avoid pushing to undefined if there are no previous tasks
+            const existingTasks = result.tasks || [];
+            var i;
+            if (existingTasks.length === 0) {
+                $("#checklist").append('<h1>There are no tasks!</h1>');
+            } else {
+                for (i = 0; i < existingTasks.length; i++) {
+                    console.log(existingTasks[i]);
+                    $("#checklist").append('<li class="list-group-item"> <div class="form-check">' +
+                        '<input type="checkbox" class="form-check-input" id="item1">' +
+                        '<label class="form-check-label" for="item1">' + existingTasks[i] + '</label>' +
+                        '</div>' +
+                        '</li>');
+                }
             }
-        }
-    })
-});
+        })
+    });
+}
--- todo_make.js
+++ todo_make.js
@@ -1,44 +1,46 @@
-$(document).ready(function() {
-    setTime();
+if (window.location.href.startsWith(chrome.runtime.getURL(''))) {
+    $(document).ready(function () {
+        setTime();
 
-    $("#todoForm").submit(function(event) {
-        // prevents default page reload
-        event.preventDefault();
+        $("#todoForm").submit(function (event) {
+            // prevents default page reload
+            event.preventDefault();
 
-        const taskTitle = $("#taskInput").val();
-        const taskDescription = $("#descriptionInput").val();
-        const taskDate = $("#dateInput").val();
-        const taskTime = $("#timeInput").val();
+            const taskTitle = $("#taskInput").val();
+            const taskDescription = $("#descriptionInput").val();
+            const taskDate = $("#dateInput").val();
+            const taskTime = $("#timeInput").val();
 
-        // require
-        if (taskTitle !== '' && taskDescription !== '' && taskDate !== '' && taskTime !== '') {
-            chrome.storage.local.get({'tasks': []}, function(result) {
-                // avoid pushing to undefined if there are no previous tasks
-                const existingTasks = result.tasks || [];
-                existingTasks.push(taskTitle);
-                // don't sync with other machines - extension is local
-                chrome.storage.local.set({'tasks': existingTasks}, function() {
-                    console.log('existingTasks', existingTasks);
-                    console.log('newTask', taskTitle);
+            // require
+            if (taskTitle !== '' && taskDescription !== '' && taskDate !== '' && taskTime !== '') {
+                chrome.storage.local.get({'tasks': []}, function (result) {
+                    // avoid pushing to undefined if there are no previous tasks
+                    const existingTasks = result.tasks || [];
+                    existingTasks.push(taskTitle);
+                    // don't sync with other machines - extension is local
+                    chrome.storage.local.set({'tasks': existingTasks}, function () {
+                        console.log('existingTasks', existingTasks);
+                        console.log('newTask', taskTitle);
+                    });
                 });
-            });
-        }
+            }
+        });
     });
-});
 
-function setTime() {
-    const dateObject = new Date();
-    const date =
-        dateObject.getDate() + "/" +
-        (dateObject.getMonth() + 1) + "/" +
-        dateObject.getFullYear();
+    function setTime() {
+        const dateObject = new Date();
+        const date =
+            dateObject.getDate() + "/" +
+            (dateObject.getMonth() + 1) + "/" +
+            dateObject.getFullYear();
 
-    const time =
-        dateObject.getHours() + ":" +
-        dateObject.getMinutes() + ":" +
-        dateObject.getSeconds();
+        const time =
+            dateObject.getHours() + ":" +
+            dateObject.getMinutes() + ":" +
+            dateObject.getSeconds();
 
-    // Set the default value using jQuery
-    $("#timeInput").val(time);
-    $("#dateInput").val(date);
+        // Set the default value using jQuery
+        $("#timeInput").val(time);
+        $("#dateInput").val(date);
+    }
 }
