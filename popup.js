0f72bb3f0e8a0bfdde8a5c68967a94d406f71ce0
Jackson Dam <2619114d@student.gla.ac.uk>
2023-11-28 00:39:37 +0000

    Closes #58: fixed checklist update on popup

--- popup.js
+++ popup.js
@@ -6,18 +6,22 @@ if (window.location.href.startsWith(chrome.runtime.getURL(''))) {
         chrome.storage.local.get({ 'tasks': [] }, function(result) {
             // avoid pushing to undefined if there are no previous tasks
             const existingTasks = result.tasks || [];
-            var i;
             if (existingTasks.length === 0) {
                 $("#checklist").append('<h1>There are no tasks!</h1>');
             }
             else {
-                for (i = 0; i < existingTasks.length; i++) {
-                    console.log(existingTasks[i]);
-                    $("#checklist").append('<li class="list-group-item"> <div class="form-check">' +
-                        '<input type="checkbox" class="form-check-input" id="item1">' +
-                        ' <div class="container">' + '<div class="row"> <label class="form-check-label" for="item1">' + existingTasks[i] + '</label>' +
-                        '<label class="form-check-label" for="item1">' + 'DATE, TIME' + '</label>' + '</div> </div>' + '</div>' + '</li>');
-                }
+				for (const taskId in existingTasks) {
+					const task = existingTasks[taskId];
+					const dueDate = new Date(task.due);
+					const formattedDueDate = dueDate.toLocaleString();
+
+					$("#checklist").append(
+						'<li class="list-group-item"> <div class="form-check">' +
+						'<input type="checkbox" class="form-check-input" id="item' + task.id + '">' +
+						' <div class="container">' + '<div class="row"> <label class="form-check-label" for="item' + task.id + '">' + task.title + '</label>' +
+						'<label class="form-check-label" for="item' + task.id + '">' + formattedDueDate + '</label>' + '</div> </div>' + '</div>' + '</li>'
+					);
+				};
             }
         });
     });
