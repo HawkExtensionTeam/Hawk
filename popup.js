39c30a1ffef363eeb4c057a045921a192514361b
Julian <2613319w@student.gla.ac.uk>
2023-11-29 02:00:24 +0000

    Closes #63: shows first 3 tasks

--- popup.js
+++ popup.js
@@ -10,18 +10,26 @@ if (window.location.href.startsWith(chrome.runtime.getURL(''))) {
                 $("#checklist").append('<h1>There are no tasks!</h1>');
             }
             else {
-				for (const taskId in existingTasks) {
-					const task = existingTasks[taskId];
-					const dueDate = new Date(task.due);
-					const formattedDueDate = dueDate.toLocaleString();
+                let taskCounter = 0;
 
-					$("#checklist").append(
-						'<li class="list-group-item"> <div class="form-check">' +
-						'<input type="checkbox" class="form-check-input" id="item' + task.id + '">' +
-						' <div class="container">' + '<div class="row"> <label class="form-check-label" for="item' + task.id + '">' + task.title + '</label>' +
-						'<label class="form-check-label" for="item' + task.id + '">' + formattedDueDate + '</label>' + '</div> </div>' + '</div>' + '</li>'
-					);
-				};
+                for (const taskId in existingTasks) {
+                    const task = existingTasks[taskId];
+                    const dueDate = new Date(task.due);
+                    const formattedDueDate = dueDate.toLocaleString();
+                
+                    $("#checklist").append(
+                        '<li class="list-group-item"> <div class="form-check">' +
+                        '<input type="checkbox" class="form-check-input" id="item' + task.id + '">' +
+                        ' <div class="container">' + '<div class="row"> <label class="form-check-label" for="item' + task.id + '">' + task.title + '</label>' +
+                        '<label class="form-check-label" for="item' + task.id + '">' + formattedDueDate + '</label>' + '</div> </div>' + '</div>' + '</li>'
+                    );
+                
+                    taskCounter++;
+                
+                    if (taskCounter === 3) {
+                        break;
+                    }
+                };
             }
         });
     });
