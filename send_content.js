6f8bbcabd0170bf071cd22f93aaa522620c25f3f
Zhixiang <2616718f@student.gla.ac.uk>
2024-01-06 19:53:36 +0000

    issue #98: send_content.js uses up-to-date jQuery syntax

--- send_content.js
+++ send_content.js
@@ -1,17 +1,15 @@
-const visibleTextContent = document.body.innerText;
-chrome.runtime.sendMessage({ action: 'sendVisibleTextContent', visibleTextContent });
+const visibleTextContent = $('body').text();
+chrome.runtime.sendMessage({action: 'sendVisibleTextContent', visibleTextContent});
 
-document.addEventListener('click', function (event) {
-    if (event.target.tagName === 'A') {
-        const link = event.target;
-        const clickedURL = new URL(link.href);
-        const currentURL = new URL(window.location.href);
+$(document).on('click', 'a', function (event) {
+    const link = $(event.target);
+    const clickedURL = new URL(link.prop('href'));
+    const currentURL = new URL(window.location.href);
 
-        const clickedURLPath = clickedURL.pathname.replace(/\/[^\/]+$/, '');
-        const currentURLPath = currentURL.pathname.replace(/\/[^\/]+$/, '');
+    const clickedURLPath = clickedURL.pathname.replace(/\/[^\/]+$/, '');
+    const currentURLPath = currentURL.pathname.replace(/\/[^\/]+$/, '');
 
-        if (clickedURL.origin === currentURL.origin && clickedURLPath === currentURLPath) {
-            chrome.runtime.sendMessage({ action: 'pageNavigated' });
-        }
+    if (clickedURL.origin === currentURL.origin && clickedURLPath === currentURLPath) {
+        chrome.runtime.sendMessage({action: 'pageNavigated'});
     }
 });
