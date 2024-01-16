d04347b2669ddc8a008082e350a5245c0265c707
William Hunt <2620286h@student.gla.ac.uk>
2024-01-16 21:39:54 +0000

    bug: configured puppeteer for GitLab CI

--- tests/index.test.js
+++ tests/index.test.js
@@ -11,6 +11,8 @@ beforeEach(async () => {
     args: [
       `--disable-extensions-except=${EXTENSION_PATH}`,
       `--load-extension=${EXTENSION_PATH}`,
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
     ],
   });
 
