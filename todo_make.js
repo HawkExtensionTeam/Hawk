326c735da70e130215d0c416974b6f371f3b5d33
Jackson Dam <2619114d@student.gla.ac.uk>
2024-02-08 06:33:26 +0000

    fix: linting warning

--- todo_make.js
+++ todo_make.js
@@ -172,7 +172,6 @@ if (window.location.href.startsWith(chrome.runtime.getURL(''))) {
         // don't sync with other machines - extension is local
         $.when(chrome.storage.local.set({ tasks: existingTasks })).done(() => {
 					chrome.alarms.create(taskId, {when: dueDate.getTime()});
-					console.log(dueDate.getTime());
           updateChecklist(existingTasks);
         });
       });
