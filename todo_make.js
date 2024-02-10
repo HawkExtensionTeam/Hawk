03c571ec87370f619dc4a8877ab0cac80e0394c1
Jackson Dam <2619114d@student.gla.ac.uk>
2024-02-10 09:00:55 +0000

    extra: make deletion modal, address x-overflow on task page body

--- todo_make.js
+++ todo_make.js
@@ -74,7 +74,7 @@ function updateChecklist(tasks) {
                 </div>
                 <div class="col-lg-2 mt-3 mt-md-3 mt-lg-0 d-flex">
                   <div class="col">
-                    <button type="button" class="btn btn-danger delete-btn fill-btn" delete-task-id="${taskId}">
+                    <button type="button" class="btn btn-danger delete-btn fill-btn" delete-task-id="${taskId}" data-bs-toggle="modal" data-bs-target="#deleteTaskModal">
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path>
                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path>
@@ -137,7 +137,7 @@ function addTaskToChecklist(taskId) {
                 </div>
                 <div class="col-lg-2 mt-3 mt-md-3 mt-lg-0 d-flex">
                   <div class="col">
-                    <button type="button" class="btn btn-danger delete-btn fill-btn" delete-task-id="${taskId}">
+                    <button type="button" class="btn btn-danger delete-btn fill-btn" delete-task-id="${taskId}" data-bs-toggle="modal" data-bs-target="#deleteTaskModal">
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path>
                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path>
@@ -234,6 +234,11 @@ if (window.location.href.startsWith(chrome.runtime.getURL(''))) {
     });
 
     $(document).on('click', '.btn.btn-danger.delete-btn', (event) => {
+      const $delBtn = $(event.currentTarget);
+      $("#confirm-delete-btn").attr('delete-task-id', $delBtn.attr('delete-task-id'));
+    });
+    
+    $(document).on('click', '.btn.btn-danger.confirm-del-btn', (event) => {
       const $delBtn = $(event.currentTarget);
       chrome.storage.local.get({ tasks: {} }, (result) => {
         const existingTasks = result.tasks || {};
