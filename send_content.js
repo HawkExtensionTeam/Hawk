0d2d855293298f5bb6c097926500ba2589a9bd66
Jackson Dam <2619114d@student.gla.ac.uk>
2024-02-21 14:13:40 +0000

    Merge branch feature/allowed-sites-list-for-indexer into main

--- send_content.js
+++ send_content.js
@@ -28,6 +28,7 @@ function checkRegexList() {
     chrome.storage.local.get(['allowedRegex'], (result) => {
       const storedRegexList = result.allowedRegex;
       const regexList = storedRegexList || [];
+
       const isMatch = regexList.some((regex) => new RegExp(regex).test(currentURL));
       resolve(isMatch);
     });
