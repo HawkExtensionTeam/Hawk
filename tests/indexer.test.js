03dc288d29328a593694382adfd512dbcf16bce1
Julian <2613319w@student.gla.ac.uk>
2024-03-05 21:41:39 +0000

    more tests

--- tests/indexer.test.js
+++ tests/indexer.test.js
@@ -54,29 +54,58 @@ test('Test if page is indexed', async () => {
   expect(linkExists).toBe(true);
 });
 
-test('Add new rule to indexing sites', async ()=>{
-    const testLink = "https://www.nba.com/games";
-    await page.goto(`chrome-extension://${EXTENSION_ID}/settings.html`);
-    await page.click("#indexing");
-    await page.click("#string-matches-tab");
-    await page.click('button[data-bs-target="#addRuleModal"]');
-    await page.waitForTimeout(2000);
-    await page.type("#addRuleInput","nba");
-    await page.click("#addRule");
+test('Add new rule to indexing sites', async () => {
+  const testLink = 'https://www.nba.com/games';
+  await page.goto(`chrome-extension://${EXTENSION_ID}/settings.html`);
+  await page.click('#indexing');
+  await page.click('#string-matches-tab');
+  await page.click('button[data-bs-target="#addRuleModal"]');
+  await page.waitForTimeout(2000);
+  await page.type('#addRuleInput', 'nba');
+  await page.click('#addRule');
 
-    let linkExists = false;
-    await page.goto(testLink);
-    await page.waitForTimeout(3000);
-    await page.goto(`chrome-extension://${EXTENSION_ID}/settings.html`);
-    const indexedData = await page.evaluate(
-      () => new Promise((resolve) => {
-        chrome.storage.local.get('indexed', (result) => {
-          resolve(result.indexed);
-        });
-      }),
-    );
-    if (indexedData != null) {
-      linkExists = indexedData.links.includes(testLink);
-    }
-    expect(linkExists).toBe(true);
+  let linkExists = false;
+  await page.goto(testLink);
+  await page.waitForTimeout(3000);
+  await page.goto(`chrome-extension://${EXTENSION_ID}/settings.html`);
+  const indexedData = await page.evaluate(
+    () => new Promise((resolve) => {
+      chrome.storage.local.get('indexed', (result) => {
+        resolve(result.indexed);
+      });
+    }),
+  );
+  if (indexedData != null) {
+    linkExists = indexedData.links.includes(testLink);
+  }
+  expect(linkExists).toBe(true);
+});
+
+test('Add delete rule to indexing sites', async () => {
+  const testLink = 'https://www.nba.com/watch/league-pass-stream';
+  await page.goto(`chrome-extension://${EXTENSION_ID}/settings.html`);
+  await page.click('#indexing');
+  await page.click('#string-matches-tab');
+  await page.waitForTimeout(2000);
+  await page.click('button.btn.btn-danger.string-matches-del');
+  await page.waitForTimeout(2000);
+  await page.waitForSelector('button.btn.btn-danger.string-matches-del');
+  await page.click('button.btn.btn-danger.string-matches-del');
+  await page.waitForTimeout(2000);
+
+  let linkExists = false;
+  await page.goto(testLink);
+  await page.waitForTimeout(3000);
+  await page.goto(`chrome-extension://${EXTENSION_ID}/settings.html`);
+  const indexedData = await page.evaluate(
+    () => new Promise((resolve) => {
+      chrome.storage.local.get('indexed', (result) => {
+        resolve(result.indexed);
+      });
+    }),
+  );
+  if (indexedData != null) {
+    linkExists = indexedData.links.includes(testLink);
+  }
+  expect(linkExists).toBe(false);
 });
