1b1d54a08865285d4ebae06fb6efe53728923a72
Mojahed Ahmed 'Abdallah Saeed Al-Rushadi <2794899a@student.gla.ac.uk>
2024-03-12 19:49:09 +0000

    Update some tests

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -5,7 +5,7 @@ let browser;
 let page;
 
 beforeAll(async () => {
-  browser = await puppeteer.launch({
+  const launchOptions = {
     headless: false,
     args: [
       `--disable-extensions-except=${EXTENSION_PATH}`,
@@ -13,22 +13,31 @@ beforeAll(async () => {
       '--no-sandbox',
       '--disable-setuid-sandbox',
     ],
-  });
+  };
 
+  browser = await puppeteer.launch(launchOptions);
+});
+
+beforeEach(async () => {
   page = await browser.newPage();
   await page.goto('chrome://extensions');
+
   EXTENSION_ID = await page.evaluate(() => {
-    const extensionsItemElement = document.querySelector('body > extensions-manager')
-      ?.shadowRoot.querySelector('#items-list')
-      ?.shadowRoot.querySelector('extensions-item');
-    return extensionsItemElement ? extensionsItemElement.getAttribute('id') : null;
+    const extensionsManager = document.querySelector('body > extensions-manager');
+    const itemsList = extensionsManager?.shadowRoot.querySelector('#items-list');
+    const extensionsItem = itemsList?.shadowRoot.querySelector('extensions-item');
+    return extensionsItem ? extensionsItem.getAttribute('id') : null;
   });
+});
+
+afterEach(async () => {
+  await page.evaluate(() => localStorage.clear());
+  await page.close();
 });
 
 afterAll(async () => {
   await browser.close();
 });
-
 test('Adding A New Note To The Extension And Verifying That Its Been Added', async () => {
   const url = `chrome-extension://${EXTENSION_ID}/add_note.html`;
   await page.goto(url);
@@ -42,7 +51,7 @@ test('Adding A New Note To The Extension And Verifying That Its Been Added', asy
     cm.setValue('This is a test note content');
   });
 
-
+  await page.waitForSelector('#add-note-button');
   await page.click('#add-note-button');
 
   await page.waitForFunction(
@@ -59,7 +68,7 @@ test('Adding A New Note To The Extension And Verifying That Its Been Added', asy
   });
 
   expect(notesCountAfter).toEqual(1); 
-});
+}, 20000);
 
 test('Persisted Notes Remain After Reloading The Page', async () => {
   await page.goto(`chrome-extension://${EXTENSION_ID}/add_note.html`);
@@ -69,43 +78,6 @@ test('Persisted Notes Remain After Reloading The Page', async () => {
   await page.waitForSelector('.note-item');
   const notesCount = await page.evaluate(() => document.querySelectorAll('.note-item').length);
   expect(notesCount).toBeGreaterThan(0);
-});
-
-
-test('Making Sure That The Notes Deletion Works as expected', async () => {
-  await page.click('#delete');
-
-  await page.waitForSelector('#confirmDelete', { visible: true });
-  await page.click('#confirmDelete');
-
-  await page.waitForFunction(() => !document.querySelector('.note-item'));
-
-  const notesCountAfter = await page.evaluate(() => {
-    const noteItems = document.querySelectorAll('.note-item');
-    return noteItems.length;
-  });
-
-  expect(notesCountAfter).toEqual(0);
-});
-
-test('Testing A note with a long content', async () => {
-  await page.goto(`chrome-extension://${EXTENSION_ID}/add_note.html`);
-
-  const longContent = 'first'.repeat(10000);
-  await page.type('#title', 'Long Content Note');
-  await page.evaluate((content) => { 
-    const cm = document.querySelector('.CodeMirror').CodeMirror;
-    cm.setValue(content);
-  }, longContent);
-  
-  await page.click('#add-note-button');
-
-  await page.waitForSelector('.note-item');
-  const notePresent = await page.evaluate(() => {
-    const noteTitle = document.querySelector('.note-item-title').textContent;
-    return noteTitle.includes('Long Content Note');
-  });
-  expect(notePresent).toBe(true);
 });
 
 test('View A Note', async () => {
@@ -118,26 +90,71 @@ test('View A Note', async () => {
   expect(noteTitle).not.toBe('');
 });
 
-test('Editing  A Note', async () => {
+test('Editing A Note', async () => {
+  const url = `chrome-extension://${EXTENSION_ID}/add_note.html`;
+  await page.goto(url);
+  
+    await page.waitForSelector('.note-item', { visible: true });
+    await page.click('.note-item');
+  
+    await page.waitForSelector('#edit', { visible: true });
+    await page.click('#edit');
+  
+    await page.waitForSelector('#title');
+    await page.type('#title', ' Edited');
+  
+    await page.waitForSelector('.CodeMirror');
+    await page.evaluate(() => {
+      const cm = document.querySelector('.CodeMirror').CodeMirror;
+      cm.setValue(' Edited');
+    });
+  
+    await page.waitForSelector('#save');
+    await page.click('#save');
+  
+    await page.waitForSelector('#titleDisplay');
+    const noteTitle = await page.evaluate(() => document.querySelector('#titleDisplay').textContent);
+    expect(noteTitle).toBe('Test Note Title Edited');
+  }, 20000);
+test('Testing A note with a long content', async () => {
   await page.goto(`chrome-extension://${EXTENSION_ID}/add_note.html`);
-  await page.waitForSelector('.note-item');
-  await page.click('.note-item');
 
-  await page.waitForSelector('#edit');
-  await page.click('#edit');
-
-  await page.waitForSelector('#title');
-  await page.type('#title', ' Edited');
-
-  await page.waitForSelector('.CodeMirror');
-  await page.evaluate(() => {
+  const longContent = 'first'.repeat(10000);
+  await page.type('#title', 'Long Content Note');
+  await page.evaluate((content) => { 
     const cm = document.querySelector('.CodeMirror').CodeMirror;
-    cm.setValue(' Edited');
+    cm.setValue(content);
+  }, longContent);
+  
+  await page.waitForSelector('#add-note-button', { visible: true });
+  await page.click('#add-note-button');
+
+  await page.waitForSelector('.note-item');
+  const notePresent = await page.evaluate(() => {
+    const noteTitle = document.querySelector('.note-item-title').textContent;
+    return noteTitle.includes('Long Content Note');
+  });
+  expect(notePresent).toBe(true);
+},20000);  
+
+test('Adding a link to a note', async () => {
+  await page.goto(`chrome-extension://${EXTENSION_ID}/add_note.html`);
+
+  await page.type('#title', 'Note with a link');
+  await page.evaluate(() => { 
+    const cm = document.querySelector('.CodeMirror').CodeMirror;
+    cm.setValue('[Link text](https://google.com)');
   });
 
-  await page.click('#save');
+  await page.waitForSelector('#add-note-button', { visible: true });
+  await page.click('#add-note-button');
 
   await page.waitForSelector('.note-item');
-  const noteTitle = await page.evaluate(() => document.querySelector('.note-item-title').textContent);
-  expect(noteTitle).toBe('Long Content Note Edited');
-});
+
+  await page.waitForSelector('.note-item:first-child', { visible: true });
+  await page.click('.note-item:first-child');
+
+  const linkHref = await page.$eval('#contentDisplay a', a => a.href);
+
+  expect(linkHref).toBe('(https://google.com)');
+}, 20000);
